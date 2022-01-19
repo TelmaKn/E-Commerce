fa2d127164cecb4a6158473b8e161b4a70dfda6d
Telma <telmakn@gmail.com>
2022-01-19 14:18:35 -0300

    Get the data from Backend, build the load and error components and add your logic

---
+++ frontend/src/components/LoadingBox.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function LoadingBox() {
+  return (
+    <div>
+      <i className="fa fa-spinner fa-spin" />
+      Loading...
+    </div>
+  );
+}
+
+export default LoadingBox;
